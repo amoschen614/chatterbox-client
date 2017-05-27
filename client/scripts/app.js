0974b86b45f77e1af66e22b39cfe80ae0a1c14d8
Amos Chen <amoschen614@gmail.com>
2017-05-27 15:42:15 -0700

    describe init behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,14 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/',
+  username: 'mackaby',
+  roomname: 'lobby',
+
+  init: function() {
+    // get username
+    app.username = window.location.search.substr(10); 
+    // cache jQuery selectors
+    app.$message = $('#message');
+    app.$chats = $('#chats');
+    app.$roomSelect = $('#roomSelect');
+    app.$send = $('#send');
+  }
